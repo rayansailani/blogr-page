ba10edd297409079000ad9616bce907bff035128
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-11-02 12:42:23 +0530

    changed image dimensions

--- src/Components/MainContent/ContentWithImageMobile.js
+++ src/Components/MainContent/ContentWithImageMobile.js
@@ -25,7 +25,7 @@ const ContentWithImageMobile = ({ info, className }) => {
   return (
     <Box className={[className, styles.flexContainer]}>
       <div className={[styles.imageContainer]}>
-        <img height="250" src={`${info["imgURL"]}mobile.svg`} />
+        <img src={`${info["imgURL"]}mobile.svg`} />
       </div>
       <div className={[styles.textContainer]}>
         <Typography
